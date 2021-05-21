94c4435213f15a04a8e89d316fd0e318b6642f03
Karol Dubas <krizz.kontakt@gmail.com>
2021-05-21 13:07:33 +0200

    Dodano TreeView z przykładowymi danymi

--- WSIiZ_WPF/MainWindow.xaml.cs
+++ WSIiZ_WPF/MainWindow.xaml.cs
@@ -24,5 +24,30 @@ namespace WSIiZ_WPF
         {
             InitializeComponent();
         }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            // Dummy data
+            var item1 = new TreeViewItem { Header = "1" };
+            var item11 = new TreeViewItem { Header = "1.1" };
+            var item111 = new TreeViewItem { Header = "1.1.1" };
+            var item112 = new TreeViewItem { Header = "1.1.2" };
+            var item1121 = new TreeViewItem { Header = "1.1.2.1" };
+            var item12 = new TreeViewItem { Header = "1.2" };
+            var item2 = new TreeViewItem { Header = "2" };
+            var item21 = new TreeViewItem { Header = "2.1" };
+            var item3 = new TreeViewItem { Header = "3" };
+
+            item112.Items.Add(item1121);
+            item11.Items.Add(item112);
+            item11.Items.Add(item111);
+            item1.Items.Add(item11);
+            item1.Items.Add(item12);
+            FolderTreeView.Items.Add(item1);
+            item2.Items.Add(item21);
+            FolderTreeView.Items.Add(item2);
+            FolderTreeView.Items.Add(item3);
+
+        }
     }
 }
