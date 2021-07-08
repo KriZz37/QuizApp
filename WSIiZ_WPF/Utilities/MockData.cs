a5a723b094f5b2636ec5a38f475cb6dc1a0e76fd
Karol Dubas <krizz.kontakt@gmail.com>
2021-07-08 22:49:31 +0200

    Dodano style dla edycji testów

--- WSIiZ_WPF/Utilities/MockData.cs
+++ WSIiZ_WPF/Utilities/MockData.cs
@@ -23,7 +23,12 @@ namespace WSIiZ_WPF.Utilities
                             new Answer { Title = "answer 1" },
                             new Answer { Title = "answer 2 answer 2 answer 2 answer 2 answer 2 answer 2" },
                             new Answer { Title = "answer 3 answer 3 answer 3" },
-                            new Answer { Title = "answer 4 answer 4 answer 4 answer 4" }
+                            new Answer 
+                            { 
+                                Title = "CORRECT answer 4 answer 4 answer 4 answer 4 answer 4 answer 4 answer 4 answer 4 answer 4 answer 4 answer 4 answer 4 answer 4 answer 4",
+                                Id = 1,
+                                Question = new Question { CorrectAnswerId = 1} 
+                            }
                         }
                     },
                     new Question
@@ -32,7 +37,12 @@ namespace WSIiZ_WPF.Utilities
                         Answers = new List<Answer>
                         {
                             new Answer { Title = "answer 1" },
-                            new Answer { Title = "answer 4" }
+                            new Answer 
+                            { 
+                                Title = "CORRECT answer 4",
+                                Id = 1,
+                                Question = new Question { CorrectAnswerId = 1}
+                            }
                         }
                     },
                     new Question
@@ -40,11 +50,109 @@ namespace WSIiZ_WPF.Utilities
                         Title = "Mock question 3?",
                         Answers = new List<Answer>
                         {
-                            new Answer { Title = "answer 1 answer 1" },
+                            new Answer 
+                            {
+                                Title = "CORRECT answer 1",
+                                Id = 1,
+                                Question = new Question { CorrectAnswerId = 1}
+                            },
                             new Answer { Title = "answer 2 answer 2 answer 2 answer 2 answer 2 answer 2" },
                             new Answer { Title = "answer 3 answer 3" },
                         }
                     },
+                };
+            }
+        }
+
+        public List<Folder> TreeFolders
+        {
+            get
+            {
+                return new List<Folder>
+                {
+                    new Folder
+                    {
+                        Title = "1",
+                        Subfolders = new List<Folder>
+                        {
+                            new Folder
+                            {
+                                Title = "1.1",
+                                Subfolders = new List<Folder>
+                                {
+                                    new Folder
+                                    {
+                                        Title = "1.1.1",
+                                        Exams = new List<Exam> { new Exam { Title = "EXAM 1.1.1" } }
+                                    }
+                                }
+                            },
+                            new Folder
+                            {
+                                Title = "1.2",
+                                Subfolders = new List<Folder>
+                                {
+                                    new Folder
+                                    {
+                                        Title = "1.2.1",
+                                        Exams = new List<Exam> { new Exam { Title = "EXAM 1.2.1" } }
+                                    },
+                                    new Folder
+                                    {
+                                        Title = "1.3.1",
+                                        Exams = new List<Exam> { new Exam { Title = "EXAM 1.3.1" } },
+                                        Subfolders = new List<Folder>
+                                        {
+                                            new Folder
+                                            {
+                                                Title = "1.3.1.1",
+                                                Exams = new List<Exam> { new Exam { Title = "EXAM 1.3.1.1" } }
+                                            },
+                                            new Folder
+                                            {
+                                                Title = "1.3.1.2",
+                                                Exams = new List<Exam> { new Exam { Title = "EXAM 1.3.1.2" } }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    },
+                    new Folder
+                    {
+                        Title = "2",
+                        Subfolders = new List<Folder>
+                        {
+                            new Folder
+                            {
+                                Title = "2.1",
+                                Subfolders = new List<Folder> { new Folder { Title = "1.1.1" } }
+                            }
+                        }
+                    },
+                    new Folder
+                    {
+                        Title = "3",
+                        Subfolders = new List<Folder>
+                        {
+                            new Folder
+                            {
+                                Title = "3.1",
+                                Subfolders = new List<Folder>
+                                {
+                                    new Folder { Title = "3.1.1" },
+                                    new Folder { Title = "3.1.2" },
+                                    new Folder
+                                    {
+                                        Title = "3.1.3",
+                                        Exams = new List<Exam> { new Exam { Title = "EXAM 3.1.3" } }
+                                    },
+                                    new Folder { Title = "3.1.4" }
+                                }
+                            }
+                        }
+                    }
                 };
             }
         }
